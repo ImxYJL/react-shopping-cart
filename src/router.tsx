f02b2ffeb1b535ef614492f7717f8eadcd37efce
ImxYJL <allensain14@gmail.com>
2024-05-31 15:25:32 +0900

    homepage 추가

--- src/router.tsx
+++ src/router.tsx
@@ -5,29 +5,34 @@ import OrderInformationOverview from './components/OrderInformationOverview/Orde
 import PaymentInfo from './components/PaymentInfo/PaymentInfo';
 import { ROUTER_URL } from './constants/constants';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          path: ROUTER_URL.MAIN,
+          element: <ShoppingCartOverview />,
+        },
+        {
+          path: ROUTER_URL.ORDER_INFO,
+          element: <OrderInformationOverview />,
+          errorElement: <div>이상해요</div>,
+        },
+        {
+          path: ROUTER_URL.PAYMENT_INFO,
+          element: <PaymentInfo />,
+          errorElement: <div>이상해요</div>,
+        },
+      ],
+      errorElement: <div>이상해요</div>,
+    },
+  ],
   {
-    path: '',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        path: ROUTER_URL.MAIN,
-        element: <ShoppingCartOverview />,
-      },
-      {
-        path: ROUTER_URL.ORDER_INFO,
-        element: <OrderInformationOverview />,
-        errorElement: <div>이상해요</div>,
-      },
-      {
-        path: ROUTER_URL.PAYMENT_INFO,
-        element: <PaymentInfo />,
-        errorElement: <div>이상해요</div>,
-      },
-    ],
-    errorElement: <div>이상해요</div>,
+    basename: 'https://ImxYJL.github.io/react-shopping-products/',
   },
-]);
+);
 
 export default router;
